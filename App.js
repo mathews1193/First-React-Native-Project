4a2c271f622e6b6aca7a06ba09d7fc59215892ab
mathews1193 <mathews1193@yahoo.com>
2020-12-25 16:40:18 -0600

    added home, login, orders, and shopping list page

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <Button
+      color="green"
+      title="Add"
+      onPress={() => alert('Add')}
+      />
+       <Button
+      color="red"
+      title="Delete"
+      onPress={() => alert('Delete')}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
